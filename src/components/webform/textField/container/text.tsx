311ae09707765035d66dd85f3b75a62760448c28
Richard Silva <rdsilva_richard@hotmail.com>
2021-10-05 12:05:04 -0300

    fix: mask textfield

--- src/components/webform/textField/container/text.tsx
+++ src/components/webform/textField/container/text.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import FieldBase from './base'
 import { TagNameEnum, FieldBaseType } from '../types/fieldBase.type'
+import { maskText } from 'shared/utils'
 
 const TextField: React.FC<FieldBaseType> = (props) => {
   const tagName: TagNameEnum = props.multiline ? TagNameEnum.textarea : TagNameEnum.input
 
-  return <FieldBase {...props} tagName={tagName} />
+  return <FieldBase
+    {...props}
+    {...(props.mask && {
+      handleBeforeChange: (value: string): string => maskText(value, props.mask || '')
+    })}
+    tagName={tagName}
+  />
 }
 
 export default TextField
