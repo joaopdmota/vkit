38c50f53aa6c9f3656b4cc14fbb68522bae47076
Richard Silva <rdsilva_richard@hotmail.com>
2021-10-08 00:06:53 -0300

    feat: textfield type

--- src/components/webform/textField/types/textField.type.ts
+++ src/components/webform/textField/types/textField.type.ts
@@ -1,12 +1,11 @@
 import { FieldType } from '../../builder/types'
 import RangeFieldType from './rangeField.type'
 import TextFieldClassesType from './textFieldClasses.type'
-import { TypeEnum } from '../enums'
 
 type TextFieldType = TextFieldClassesType &
   FieldType &
   RangeFieldType & {
-    type?: TypeEnum
+    type?: string
     disabled?: boolean
     mask?: string
   }
